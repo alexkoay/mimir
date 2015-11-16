3a6a8cc90463f1116cef2eb3b108e816a31f61c8
Alex Koay <alexkoay88@gmail.com>
2015-11-16 13:22:32 +0800

    Refactor query parameter handling into a subcomponent.

---
+++ client/src/panel/data/command.ts
@@ -0,0 +1,82 @@
+import {State} from '../panel';
+
+var key = 0;
+export class Parameter {
+	private parent: number = key;
+	key: string;
+	name: string;
+	value: string | number = null;
+	type: string = null;
+	force: boolean = false;
+
+	constructor(args: any, old: {[key: string]: any}) {
+		if (typeof args === 'string') { this.key = args; }
+		else {
+			this.key = args.key;
+			this.name = args.name || null;
+			this.value = args.value || null;
+			this.type = args.type || null;
+			this.force = args.force || false;
+		}
+
+		this.key = this.key.toString();
+		this.name = this.name || this.key;
+		if (this.value && typeof this.value !== 'string') { this.value = this.value.toString(); }
+
+		if (!this.force && old && old[this.key] !== undefined) { this.value = old[this.key]; }
+	}
+	parameter() { return '%(' + this.key.replace('%', '').replace('(', '').replace(')', '') + ')s'; }
+	toJSON() { return this.value; }
+
+	update(value: string) { this.value = value; }
+	view(parent: number) {
+		return m('span.param',
+			m('label', {for: parent + '-cmd-' + this.key}, this.name),
+			m('input', {id: parent + '-cmd-' + this.key,
+				oninput: (e: Event) => this.update((<HTMLInputElement> e.target).value),
+				value: this.value})
+		)
+	}
+}
+
+export default class Command {
+	private key: number = ++key;
+	private restore: {[key: string]: any};
+	plain: boolean;
+	cmd: string;
+	edit: boolean = true;
+	param: {[key: string]: Parameter};
+	order: string[];
+
+	constructor(state: State) {
+		console.log(state);
+		this.restore = state['param'] || {};
+		this.edit = state['edit'] || true;
+		eval('this.process`' + state['cmd'] + '`');
+	}
+	private process(cmds: string[], ...args: any[]) {
+		if (cmds.length == 1) {
+			this.plain = true;
+			this.cmd = cmds[0];
+		}
+		else {
+			this.plain = false;
+
+			this.param = {};
+			this.cmd = cmds[0];
+			args.forEach((arg: any, i: number) => {
+				var p = new Parameter(arg, this.restore);
+				if (!(p.key in this.param)) { this.param[p.key] = p; }
+				else { p = this.param[p.key]; }
+
+				this.cmd += p.parameter();
+				this.cmd += cmds[i+1];
+			});
+		}
+		console.log(this.cmd);
+	}
+	state() { return {cmd: this.cmd, edit: this.edit, param: this.param}; }
+
+	get completed() { return this.plain || Object.keys(this.param).every(key => !!this.param[key].value); }
+	view(attr: any): any { return !this.plain && this.edit ? m('form', attr, Object.keys(this.param).map(key => this.param[key].view(this.key))) : null; }
+}
--- client/src/panel/data/query.ts
+++ client/src/panel/data/query.ts
@@ -2,46 +2,46 @@ import Socket from '../../socket';
 import Data from '../../data/query';
 import Panel, {State, NodeList} from '../panel';
 
+import Command from './command';
+
 const MAX_PREVIEW = 30;
 
 export default class Query extends Panel {
 	private socket: Socket = null;
 	name: string;
 	cmd: string;
-
-	params: {[key: string]: any};
-	private $parsed: string;
-	private $order: string[];
-	private $temp: {[key: string]: any};
+	private $cmd: Command;
 
 	private data: Data;
 
 	constructor(state: State) {
 		super(state);
 		this.cmd = state['cmd'];
-		this.params = state['params'] || null;
 		this.name = state['name'] || null;
-		this.process();
+		this.$cmd = new Command(state);
 	}
 	inherit(parent: NodeList, will: any) {
 		super.inherit(parent, will);
 		if (this.socket != will.socket) {
 			this.socket = will.socket;
-			this.data = new Data(this.socket, this.$parsed);
+			this.data = new Data(this.socket, this.command);
 			this.data.onchange = () => m.redraw();
 			if (!this.dead()) { this.query(); }
 		}
 	}
 	will() { return {socket: this.socket, data: this.data}; }
 	dead() { return this.$close; }
-	state() { return super.state().include({name: this.name, cmd: this.cmd, params: this.params}); }
+	state() { return super.state().include(this.$cmd.state()).include({name: this.name, cmd: this.cmd}); }
 
 	// model ///////////////////////////////////////////////////////////////////
 
+	get plain() { return this.$cmd.plain; }
+	get queryable() { return this.$cmd.completed; }
 	get pending() { return this.data.pending; }
 	get cancelled() { return this.data.cancelled; }
 	get completed() { return this.data.completed; }
 
+	get command() { return this.$cmd.cmd; }
 	get preview() {
 		if (this.name) { return this.name; }
 		var lines = this.cmd.split('\n');
@@ -50,51 +50,15 @@ export default class Query extends Panel {
 		return first;
 	}
 
-	get paramsPopulated() { return !this.params || Object.keys(this.params).every(key => !!this.params[key]); }
-
 	get loadedRows() { return this.data.rows.length; }
 	get totalRows() { return this.data.count; }
 
 	// controller //////////////////////////////////////////////////////////////
 
-	process() {
-		try { eval('this._process`' + this.cmd + '`'); }
-		catch (e) { window.setTimeout(() => { this.fail(e.toString()); m.redraw(); }, 0); }
-	}
-	private _process(cmds: string[], ...args: any[]) {
-		if (cmds.length == 1) { this.$parsed = this.cmd; }
-		else {
-			let old = this.params || {};
-			this.params = {};
-			this.$order = args.map((arg: any) => {
-				var key: string, val: any = null, init: boolean = true;
-				if (typeof arg === 'string') { key = arg; }
-				else {
-					key = arg.key;
-					val = arg.val.toString();
-					init = arg.init;
-				}
-
-				// prefer value in the command string than saved data
-				if (!init) { val = (old[key] !== undefined ? old[key] : val); }
-
-				this.params[key] = val;
-				return key;
-			});
-
-			this.$parsed = cmds[0];
-			for (let i=0; i<this.$order.length; ++i) {
-				this.$parsed += '%(' + this.$order[i] + ')s';
-				this.$parsed += cmds[i+1];
-			}
-		}
-	}
-	update(key: string, value: any) { this.params[key] = value; }
-
 	query() {
 		if (this.dead()) { return; }
-		if (this.params && !this.paramsPopulated) { return; }
-		this.data.query(this.params)
+		if (!this.queryable) { return; }
+		this.data.query(this.$cmd.param)
 			.then(this.table.bind(this, true), this.fail.bind(this))
 			.catch(console.log.bind(console, 'error'));
 	}
@@ -122,9 +86,7 @@ export default class Query extends Panel {
 			return super.view({tree: !this.pending},
 				Panel.toolbar([
 					!this.pending && (this.cancelled || this.completed)
-						? (this.params
-							? m('button', {onclick: this.query.bind(this), disabled: !this.paramsPopulated}, Panel.icon('e/magnifying-glass'))
-							: m('button', {onclick: this.query.bind(this)}, Panel.icon('e/cycle')))
+						? m('button', {onclick: this.query.bind(this), disabled: !this.queryable}, Panel.icon(this.plain ? 'e/cycle' : 'e/magnifying-glass'))
 						: m('button', {onclick: this.stop.bind(this)}, Panel.icon('e/controller-stop')),
 					m('span.cmd', this.preview)
 				], [
@@ -132,14 +94,7 @@ export default class Query extends Panel {
 					m('button', {onclick: () => this.table()}, 'View Data'),
 					m('button', {onclick: () => this.data.xlsx()}, 'Export')
 				]),
-				!this.params ? null : m('form', {onsubmit: (e: Event) => { e.preventDefault(); this.query(); }},
-					Object.keys(this.params).map(key =>
-						m('span.param',
-							m('label', {for: this.key + '-param-' + key}, key),
-							m('input', {id: this.key + '-param-' + key, oninput: (e: Event) => this.update(key, (<HTMLInputElement> e.target).value), value: this.params[key]})
-						)
-					)
-				)
+				this.$cmd.view({onsubmit: (e: Event) => { e.preventDefault(); this.query(); }})
 			);
 		}
 	}
