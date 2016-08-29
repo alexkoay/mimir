e981490b215dbdd187b2a790457c9013142cfacd
Alex Koay <alexkoay88@gmail.com>
2016-08-29 10:54:11 +0800

    Remove debug for websocket.

--- source/js/socket.ts
+++ source/js/socket.ts
@@ -1,4 +1,4 @@
 import Socket, { Context } from './lib/socket';
 
-export default new Socket('ws://' + location.host + '/socket', true);
+export default new Socket('ws://' + location.host + '/socket');
 export { Socket, Context };
