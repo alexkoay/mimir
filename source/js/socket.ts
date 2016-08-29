5e612fa3f83a3495c720ef678c3714f07ca18960
Alex Koay <alexkoay88@gmail.com>
2016-08-29 10:52:21 +0800

    Fixed websocket constructor pointing to localhost.

--- source/js/socket.ts
+++ source/js/socket.ts
@@ -1,4 +1,4 @@
 import Socket, { Context } from './lib/socket';
 
-export default new Socket('ws://localhost:8765', true);
+export default new Socket('ws://' + location.host + '/socket', true);
 export { Socket, Context };
