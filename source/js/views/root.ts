bf8bf59bf7eeb9c8bff040f066341d9df9375f20
Alex Koay <alexkoay88@gmail.com>
2016-08-30 21:57:15 +0800

    Remove redundant code from old iterations.

--- source/js/views/root.ts
+++ source/js/views/root.ts
@@ -9,23 +9,14 @@ import store from '../store';
 export default {
 	modal: function(show: string) { this.$modal = show; },
 	keydown: function(e: KeyboardEvent) {
-		var redraw = false;
 		if (e.altKey) {
-			if (this.$modal) { }
-			else if (e.key == '1') { this.modal('reports'); redraw = true; }
-			else if (e.key == '2') { this.modal('saved'); redraw = true; }
-			else if (e.key == '3') { this.modal('recent'); redraw = true; }
-			else if (e.key == 'r' || e.key == 'R') { store.dispatch({type: 'retry'}); } 
+			if (e.key == 'r' || e.key == 'R') { store.dispatch({type: 'retry'}); } 
 			else if (e.key == 'c' || e.key == 'C') { store.dispatch({type: 'cancel'}); }
 			else if (e.key == 's' || e.key == 'S') { store.dispatch({type: 'save'}); }
 		}
-		if (this.$modal && e.key == 'Escape') { this.modal(null); redraw = true; } 
-
-		if (redraw) { m.redraw(); }
 	},
 
 	oninit: function(n: MithrilVNode) {
-		this.$modal = null;
 		document.addEventListener('keydown', this.keydown.bind(this));
 	},
 	page: function(): MithrilChild {
@@ -37,7 +28,7 @@ export default {
 
 	view: function(n: MithrilVNode) {
 		var state = store.getState();
-		var page = this.page(state.view);
+			var page = this.page(state.view);
 		return [
 			m(Header),
 			m(Query, {class: page !== null ? 'remove' : ''}),
